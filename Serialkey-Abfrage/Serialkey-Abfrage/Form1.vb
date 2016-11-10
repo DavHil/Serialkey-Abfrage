f28e3de8c4a92e9a483c5cb39c069c0cfae06821
David Hilberath <David-Hilberath@hotmail.de>
2016-11-10 15:27:16 +0100

    Projekt erstellt

---
+++ Serialkey-Abfrage/Serialkey-Abfrage/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Dim Web As New System.Net.WebClient
+    Dim EnteredSerial As String = ""
+    Dim Serials As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        checkserial()
+    End Sub
+
+    Sub checkserial()
+1:      EnteredSerial = InputBox("Bitte geben Sie hier Ihren Serial Key ein!", "Serialkey")
+        If EnteredSerial = "" Then GoTo 1
+
+        Serials = Web.DownloadString("file:///C:/xampp/htdocs/Serials.txt")
+        If Not Serials.Contains(EnteredSerial) Then
+            MsgBox("Fehlgeschlagen")
+            Application.Exit()
+        Else
+            MsgBox("Der Key war gültig")
+        End If
+    End Sub
+End Class
+
---
+++ Serialkey-Abfrage/Serialkey-Abfrage/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Serialkey-Abfrage")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Serialkey-Abfrage")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("79441af5-39ea-4c73-96ba-ea93be60a448")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
